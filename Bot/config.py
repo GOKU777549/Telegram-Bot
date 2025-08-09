0329a1e9a461ce73710d2a6bf8f57c25fa83affa
GOKU777549 <b7774455@gmail.com>
2025-08-09 06:10:46 +0530

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -5,10 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    API_ID = int(os.getenv("API_ID", "6"))
-    API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7283682714:AAFnVmikIg2mgnmw7Vjze3bfM-Ew6wb8bMk")
+    API_ID = int(os.getenv("API_ID", "21218274"))
+    API_HASH = os.getenv("API_HASH", "3474a18b61897c672d315fb330edb213")
     DB_URI = os.getenv("DB_URI", "sqlite:///bot.db")
-    MONGO_URI = os.getenv("MONGO_URI", os.getenv("MONGO_URL", ""))
-    DB_NAME = os.getenv("DB_NAME", "sample")
 
+    # MongoDB Settings
+    MONGO_URI = os.getenv(
+        "MONGO_URI",
+        "mongodb+srv://sufyan532011:5042@auctionbot.5ms20.mongodb.net/?retryWrites=true&w=majority&appName=AuctionBot"
+    )
+    DB_NAME = os.getenv("DB_NAME", "auctionbot")
